a45620b3e3d6a0b1f464bae52db593abd9406ce2
haxi0 <fabiomailboxgood@mail.ru>
2023-06-01 23:05:13 +0300

    Initial Commit

---
+++ ballbr3aker/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ballbr3aker
+//
+//  Created by Анохин Юрий on 01.06.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ballbr3aker/ballbr3akerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ballbr3akerApp.swift
+//  ballbr3aker
+//
+//  Created by Анохин Юрий on 01.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ballbr3akerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
